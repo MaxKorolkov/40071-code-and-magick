0c7a9de38068d4d97ed8b2d177cd91393f8f24ee
Maxim Korolkov <maximkorolkov1@gmail.com>
2017-04-14 01:38:20 +0300

    First commit

--- js/setup.js
+++ js/setup.js
@@ -42,8 +42,6 @@ function getWizardsArray(number) {
 // объявление массива похожих персонажей и заполнение через функцию с количеством эллементов;
 var wizards = getWizardsArray(4);
 
-// Показ диалога инвентаря
-document.querySelector('.setup').classList.remove('hidden');
 
 // переменные списка похожих персонажей и шаблона персонажа
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -67,3 +65,93 @@ similarListElement.appendChild(fragment);
 
 // показ списка похожих персонажей
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+// переменые для работы с окном персонажа
+var setup = document.querySelector('.setup');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupClose = setup.querySelector('.setup-close');
+var formUserInput = document.forms[0].elements[0];
+var focusUserInput = false;
+
+// обработчики фокуса на поле ввода имени пользователя
+formUserInput.addEventListener('focus', function () {
+  focusUserInput = true;
+});
+
+formUserInput.addEventListener('blur', function () {
+  focusUserInput = false;
+});
+
+// функция закрытия окна персонажа
+function closePopup() {
+  setup.classList.add('hidden');
+}
+
+// обработчик нажатия Esc для закрытия окна персонажа
+function onPopupEscPress(evt) {
+  if (evt.keyCode === 27 && !focusUserInput) {
+    closePopup();
+  }
+}
+
+// закрытие окна при фокусе на иконку закрытия и нажатия Enter
+function onSetupCloseEnter(evt) {
+  if (evt.keyCode === 13) {
+    closePopup();
+  }
+}
+
+// функция открытия окна персонажа
+function openPopup() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+  setupClose.addEventListener('keydown', onSetupCloseEnter);
+}
+
+// обработчик клика по иконке пользователя
+setupOpenIcon.addEventListener('click', openPopup);
+
+// открытие персонажа при фокусе на иконке пользователя и нажатии Enter
+function onIconEnterPress(evt) {
+  if (evt.keyCode === 13) {
+    openPopup();
+  }
+}
+
+// обработчик нажатия Enter при фокусе на иконке пользователя
+setupOpenIcon.addEventListener('keydown', onIconEnterPress);
+
+// обработчик закрытия формы
+setupClose.addEventListener('click', closePopup);
+
+// объект с разными вариантами цветов для персонажа
+var wizardColors = {
+  'coat': ['rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ],
+  'eyes': ['black', 'red', 'blue', 'yellow', 'green'],
+  'fireball': ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
+};
+
+// переменные элементов персонажа
+var wizardCoat = document.querySelector('.wizard-coat');
+var wizardEyes = document.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+// обработчики событий на нажатие элементов персонажа
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = randomArrayElement(wizardColors.coat);
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = randomArrayElement(wizardColors.eyes);
+});
+
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = randomArrayElement(wizardColors.fireball);
+});
+
